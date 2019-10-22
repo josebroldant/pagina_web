93d09205bafb49521319535314525eda980e0a8d
josebroldant@gmail.com <josebroldant@gmail.com>
2019-10-22 10:54:40 -0500

    fixed connection to mongo

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var net = require('net');//libreria net
 var express = require('express');//libreria express
-    app = express();//metodo constructor de express, express as framework to use http easily
-    bodyParser = require("body-parser");
-    methodOverride = require("method-override");
-    mongodb = require('mongodb');
+app = express();//metodo constructor de express, express as framework to use http easily
+bodyParser = require("body-parser");
+methodOverride = require("method-override");
+mongodb = require('mongodb');
 var timeout = require('connect-timeout');
 var session = require('express-session');
 var timeoutID;
 
-app.use(bodyParser.urlencoded({extended:false}));
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-var router=express.Router();
+var router = express.Router();
 
 var users = ['nickname']
-var userspass =['pass']
+var userspass = ['pass']
 
 /*app.get('/home1', function(req,res){
     var usuario = req.query.user;
@@ -29,38 +31,38 @@ var userspass =['pass']
    
 });*/
 
-router.post('/users', (req, res)=>{
+router.post('/users', (req, res) => {
     var usuario = req.body.user;
-    var contra  = req.body.pass;
-    if((users[0] == usuario) && (userspass[0] == contra)){
+    var contra = req.body.pass;
+    if ((users[0] == usuario) && (userspass[0] == contra)) {
         res.sendfile(__dirname + '/public/index2.html');
     }
-    else{
+    else {
         res.sendfile(__dirname + '/public/formulario.html');
-        users.push(req.body.user+users.length)//guardar parametros
-        userspass.push(req.body.pass+userspass.length)//guardar parametros
+        users.push(req.body.user + users.length)//guardar parametros
+        userspass.push(req.body.pass + userspass.length)//guardar parametros
         res.sendfile(__dirname + '/public/index.html');
     }
-    
+
 });
 
 app.use(router);
 
-router.get('/home', function(req,res){
+router.get('/home', function (req, res) {
     res.sendfile(__dirname + '/public/index.html');
 });
 
-router.get('/home2', function(req,res){
+router.get('/home2', function (req, res) {
     res.sendfile(__dirname + '/public/index2.html');
 });
 
-router.get('/home3', function(req,res){
+router.get('/home3', function (req, res) {
     res.sendfile(__dirname + '/public/index3.html');
 });
 
 //app.use(timeout('10s'));
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Ready");
 });
 
@@ -70,13 +72,52 @@ app.use(express.static('public/css'));//acceso a css
 app.use(express.static('public/js'));//acceso a js
 console.log('Página habilitada');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://jose.roldan@correo.usa.edu.co:#Exeron97@ssmcluster-aobqi.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const uri = "mongodb+srv://ssmcluster-aobqi.mongodb.net/test";
+mongoose.connect(uri, { useNewUrlParser: true })
+    .then(() => {
+        console.log("Successfuly conected");
+    })
+    .catch((err) => console.error(err));
+
+//data declaration
+class datos{
+    constructor(datos) {
+        this.voltaje = voltaje;
+        this.corriente = corriente;
+        this.potencia = potencia;
+        this.estado = estado;
+        this.llenado = llenado;
+    }
+} 
+
+voltaje=0;
+corriente=0;
+potencia=0;
+estado='ok';
+llenado=0;
+
+// POST method route
+app.post('mongodb://[jose.roldan@correo.usa.edu.co:#Exeron97@]localhost[:3000][/[variables][?options]]', function (req, res) {
+    res.send(voltaje);
+    res.send(corriente);
+    res.send(potencia);
+    res.send(estado);
+    res.send(llenado);
+    console.log("Posted succesfully");
 });
+
+// const MongoClient = require('mongodb').MongoClient;
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect()
+//     .then(() => {
+//         console.log("Successfuly conected");
+
+//     })
+
+// client.connect(err => {
+//   const collection = client.db("variables").collection("container");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
